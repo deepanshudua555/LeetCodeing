0a06dc3cd1dc85c94325e482f9e5947d60259a8a
deepanshudua555 <93878722+deepanshudua555@users.noreply.github.com>
2022-12-15 07:48:24 +0530

    Time: 24 ms (63.79%), Space: 52.4 MB (7.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -11,25 +11,55 @@ class Solution {
 //     }
     
     public int longestCommonSubsequence(String text1, String text2) {
-       
-         int n1 = text1.length();
+        int n1 = text1.length();
         int n2 = text2.length();
-        int[]prev = new int[n2+1];
-        int[]curr = new int[n2+1];
-       
+        Integer[][]dp = new Integer[n1+1][n2+1];
+        for(int i=0; i<=n1;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=n2;j++){
+            dp[0][j] = 0;
+        }
         for(int idx1=1;idx1<=n1;idx1++){
             for(int idx2=1;idx2<=n2;idx2++){
              if(text1.charAt(idx1-1)==text2.charAt(idx2-1)){
-                curr[idx2] = 1+prev[idx2-1];
+                dp[idx1][idx2] = 1+dp[idx1-1][idx2-1];
             }
-            else curr[idx2] = 0+Math.max(prev[idx2],curr[idx2-1]);   
+            else dp[idx1][idx2] = 0+Math.max(dp[idx1-1][idx2],dp[idx1][idx2-1]);   
             }
-            prev=(int[])(curr.clone());
         }
-        return prev[n2];
+        
+        int len = dp[n1][n2];
+        int i=n1;
+        int j=n2;
+        int index = len-1;
+        String ans = "";
+        // for(int i=0;i<len; i++){
+        //     str+='$';
+        // }
+        
+        while(i>0&&j>0){
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                ans+=text1.charAt(i-1);
+                index--;
+                i--;j--;
+            }
+            else if (dp[i-1][j]>dp[i][j-1]){
+                i--;
+            }
+            else j--;
+        }
+        System.out.println(ans);
+
+        
+        
+        // for(int i=0; i<=n1; i++){
+        //     for(int j=0;j<=n2;j++){
+        //         System.out.print(dp[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println(ans);
+        return dp[n1][n2];
     }
-
-    
-
-
 }
