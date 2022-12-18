345fcff4f0f276d65e10406aafa534a4c89df126
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2022-12-18 14:04:58 +0530

    Time: 1 ms (99.83%), Space: 58.2 MB (8.86%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public void reverseString(char[] s) {
+        // char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
