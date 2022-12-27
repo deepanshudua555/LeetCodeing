bcb4bb61e8e8d34126814ddb1f4a9316c38b45f7
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2022-12-27 22:40:55 +0530

    Time: 12 ms (41.97%), Space: 45.2 MB (29.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -12,8 +12,18 @@ class Solution {
         int n  = str1.length();
         int m = str2.length();
         Integer[][]dp = new Integer[n+1][m+1];
-
-        return f(n,m,str1,str2,dp);
+        for(int i=0; i<=n; i++)dp[i][0] = i;
+        for(int j=0; j<=m; j++)dp[0][j] = j;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1))dp[i][j] = dp[i-1][j-1];
+                else{
+                  dp[i][j] = 1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));  
+                }
+            }
+        }
+        return dp[n][m];
         
     }
 }
