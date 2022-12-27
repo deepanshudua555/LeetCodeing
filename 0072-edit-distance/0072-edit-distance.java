25cd21f145645dfeca94a0c64ded30d810c7ecc3
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2022-12-27 22:33:52 +0530

    Time: 3 ms (100.00%), Space: 42.4 MB (82.53%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,19 +1,19 @@
 class Solution {
     
     public int f(int i,int j,String  str1,String str2,Integer[][]dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
+        if(i==0)return j;
+        if(j==0)return i;
         if(dp[i][j]!=null)return dp[i][j];
-        if(str1.charAt(i)==str2.charAt(j))return dp[i][j] = f(i-1,j-1,str1,str2,dp);
+        if(str1.charAt(i-1)==str2.charAt(j-1))return dp[i][j] = f(i-1,j-1,str1,str2,dp);
         return dp[i][j] = 1+Math.min(f(i,j-1,str1,str2,dp),Math.min(f(i-1 ,j,str1,str2,dp),f(i-1,j-1,str1,str2,dp)));
     }
     
     public int minDistance(String str1, String str2) {
         int n  = str1.length();
         int m = str2.length();
-        Integer[][]dp = new Integer[n][m];
+        Integer[][]dp = new Integer[n+1][m+1];
 
-        return f(n-1,m-1,str1,str2,dp);
+        return f(n,m,str1,str2,dp);
         
     }
 }
