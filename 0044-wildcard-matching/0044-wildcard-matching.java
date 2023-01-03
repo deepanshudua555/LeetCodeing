fbf4a3f3e9f81a0e204d7c294d8f369f88f66947
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2023-01-03 11:36:45 +0530

    Time: 61 ms (17.59%), Space: 54.2 MB (18.51%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,20 +2,20 @@ class Solution {
     
     public boolean f(int i,int j, String p, String s,Boolean[][] dp){
         // System.out.println("H");
-        if(i<0 && j<0)return true ;
-        if(i<0 && j>=0)return false;
-        if(j<0 && i>=0){
-            for(int ii =0;ii<=i;ii++){
-                if(p.charAt(ii)!='*')return false;
+        if(i==0 && j==0)return true ;
+        if(i==0 && j>0)return false;
+        if(j==0 && i>0){
+            for(int ii =1;ii<=i;ii++){
+                if(p.charAt(ii-1)!='*')return false;
             }
             return true;
         }
         
         if(dp[i][j]!=null)return dp[i][j];
-        if(p.charAt(i)==s.charAt(j) || p.charAt(i)=='?'){
+        if(p.charAt(i-1)==s.charAt(j-1) || p.charAt(i-1)=='?'){
             return dp[i][j]= f(i-1,j-1,p,s,dp);
         }
-        if(p.charAt(i)=='*'){
+        if(p.charAt(i-1)=='*'){
             return dp[i][j] =  f(i,j-1,p,s,dp)|f(i-1,j,p,s,dp);
         }
         return dp[i][j]=false;
@@ -23,8 +23,9 @@ class Solution {
     
     public boolean isMatch(String s, String p) {
         int n = p.length();
-        int m = s.length();
-        Boolean[][] dp = new Boolean[n][m];
-        return f(n-1,m-1,p,s,dp);
+        int m = s.length(); 
+        Boolean[][] dp = new Boolean[n+1][m+1];
+        
+        return f(n,m,p,s,dp);
     }
 }
