9fe9341e347f0c1a7656055dc5f50875cdd9f3f3
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2023-02-22 23:58:04 +0530

    Time: 114 ms (22.51%), Space: 144.2 MB (5.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -9,18 +9,18 @@ class Solution{
             for(int buy = 0; buy<=1; buy++){
                 for(int cap=1; cap<=2; cap++){
 
-                    if(buy==0){// We can buy the stock
-                        dp[ind][buy][cap] = Math.max(0+dp[ind+1][0][cap], 
-                                    -prices[ind] + dp[ind+1][1][cap]);
+                    if(buy==1){// We can buy the stock
+                        dp[ind][buy][cap] = Math.max(0+dp[ind+1][1][cap], 
+                                    -prices[ind] + dp[ind+1][0][cap]);
                      }
 
-                    if(buy==1){// We can sell the stock
-                        dp[ind][buy][cap] = Math.max(0+dp[ind+1][1][cap],
-                                    prices[ind] + dp[ind+1][0][cap-1]);
+                    if(buy==0){// We can sell the stock
+                        dp[ind][buy][cap] = Math.max(0+dp[ind+1][0][cap],
+                                    prices[ind] + dp[ind+1][1][cap-1]);
                     }
                 }
             }
         }
-        return dp[0][0][2];
+        return dp[0][1][2];
     }
 }
