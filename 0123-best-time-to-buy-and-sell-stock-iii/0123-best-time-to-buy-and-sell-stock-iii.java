c2f695dd558580446267eeca2ca9636aa3389ff2
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2023-02-22 23:51:44 +0530

    Time: 112 ms (22.74%), Space: 144 MB (5.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,50 +1,26 @@
-// class Solution {
-  
-//     public int f(int ind, int buy, int cap, int[] p, int n){
-//        if(ind==n||cap==0)return 0;
+class Solution{
+	 public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][][] = new int[n+1][2][3];
         
-//         if(buy==1){
-//             return Math.max(-p[ind]+f(ind+1,0,cap,p,n), 0+f(ind+1, 1, cap,p,n));
-//         }
-//         return Math.max(p[ind]+f(ind+1,1,cap-1,p,n),0+f(ind+1,0,cap,p,n));
-//     }
-    
-//     public int maxProfit(int[] prices) {
-//         int n = prices.length;
-//         return f(0,1,2,prices,n);
-//     }
-// }
+        // As dp array is intialized to 0, we have already covered the base case
+        
+        for(int ind = n-1; ind>=0; ind--){
+            for(int buy = 0; buy<=1; buy++){
+                for(int cap=1; cap<=2; cap++){
 
+                    if(buy==0){// We can buy the stock
+                        dp[ind][buy][cap] = Math.max(0+dp[ind+1][0][cap], 
+                                    -prices[ind] + dp[ind+1][1][cap]);
+                     }
 
-class Solution {
-	int dp[][][];
-		public int maxProfit(int[] prices) {
-			int n = prices.length;
-			dp = new int[n][2][3];
-
-			for (int i=0;i<n;i++){
-				for (int j=0;j<2;j++) Arrays.fill(dp[i][j],-1);
-			}
-
-			return find_max_profit(0,1,prices,2,n);
-		}
-		private int find_max_profit(int ind,int buy,int[] prices,int cap,int n){
-			if (ind == n || cap == 0) return 0;
-
-			if (dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
-
-			// if buy = 1 we need to buy anything
-			if (buy == 1){
-				int buying = -prices[ind] + find_max_profit(ind+1,0,prices,cap,n);
-				int not_buying = find_max_profit(ind+1,1,prices,cap,n);
-				return dp[ind][buy][cap] = Math.max(buying,not_buying);
-			}
-			// if buy = 0  then we have already buy anything, this time we need to sell
-			else
-			{
-				int selling = prices[ind] + find_max_profit(ind+1,1,prices,cap-1,n);
-				int not_selling = find_max_profit(ind+1,0,prices,cap,n);
-				return dp[ind][buy][cap] = Math.max(selling,not_selling);
-			}
-		}
-	}
+                    if(buy==1){// We can sell the stock
+                        dp[ind][buy][cap] = Math.max(0+dp[ind+1][1][cap],
+                                    prices[ind] + dp[ind+1][0][cap-1]);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
+    }
+}
