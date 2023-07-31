d264e1b4d08489dade8cc54cb9dc22763af96481
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2023-07-31 15:40:35 +0530

    Time: 10 ms (97.78%), Space: 41.6 MB (45.18%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,23 +1,24 @@
 class Solution {
-    public static int f(int i, int j, int[] newCuts, Integer dp[][]){
-        
-        if(i>j)return 0;
-        if(dp[i][j]!=null)return dp[i][j];
-        int mini = Integer.MAX_VALUE;
-        for(int ind=i; ind<=j; ind++){
-            int cost = newCuts[j+1]-newCuts[i-1]+f(i,ind-1,newCuts,dp)+f(ind+1,j,newCuts,dp);
-            mini = Math.min(mini,cost);
-        }
-        return dp[i][j] = mini;
-    }
-    
     public int minCost(int n, int[] cuts) {
         int m = cuts.length;
-        int newCuts[] = new int[m + 2];
+        int[] newCuts = new int[m + 2];
         System.arraycopy(cuts, 0, newCuts, 1, m);
         newCuts[m + 1] = n;
         Arrays.sort(newCuts);
-        Integer dp[][] = new Integer[m+2][m+2];
-        return f(1,cuts.length,newCuts,dp);
-    }
+        
+        int[][] dp = new int[m + 2][m + 2];
+        
+        for (int diff = 2; diff < m + 2; ++diff) {
+            for (int left = 0; left < m + 2 - diff; ++left) {
+                int right = left + diff;
+                int ans = Integer.MAX_VALUE;
+                for (int mid = left + 1; mid < right; ++mid) {
+                    ans = Math.min(ans, dp[left][mid] + dp[mid][right] + newCuts[right] - newCuts[left]);
+                }
+                dp[left][right] = ans;
+            }
+        } 
+        
+        return dp[0][m + 1];
+    }    
 }
