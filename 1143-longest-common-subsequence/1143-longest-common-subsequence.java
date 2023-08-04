92ec77b99bd8e6e792b46137867ab06e3da86ad4
Deepanshu Dua <93878722+deepanshudua555@users.noreply.github.com>
2023-08-05 00:19:22 +0530

    Time: 31 ms (71.13%), Space: 57.8 MB (5.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,65 +1,16 @@
 class Solution {
     
-//     public int helper(int idx1,int idx2,String t1,String t2,Integer[][]dp){
-//         if(idx1==0|| idx2==0)return 0;
-//         if(dp[idx1][idx2]!=null)return dp[idx1][idx2];
-//         if(t1.charAt(idx1-1)==t2.charAt(idx2-1)){
-//             return dp[idx1][idx2] = 1+helper(idx1-1,idx2-1,t1,t2,dp);
-//         }
-//         return dp[idx1][idx2] = 0+Math.max(helper(idx1-1,idx2,t1,t2,dp),helper(idx1,idx2-1,t1,t2,dp));
-        
-//     }
+    public int f(int i, int j, String s1, String s2, Integer dp[][]){
+        if(i==s1.length()||j==s2.length())return 0;
+        if(dp[i][j]!=null)return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+            return dp[i][j] =  1+f(i+1,j+1, s1,s2,dp);
+        }
+        return dp[i][j] = Math.max(f(i+1,j,s1,s2,dp), f(i,j+1,s1,s2,dp));
+    }
     
     public int longestCommonSubsequence(String text1, String text2) {
-        int n1 = text1.length();
-        int n2 = text2.length();
-        Integer[][]dp = new Integer[n1+1][n2+1];
-        for(int i=0; i<=n1;i++){
-            dp[i][0]=0;
-        }
-        for(int j=0;j<=n2;j++){
-            dp[0][j] = 0;
-        }
-        for(int idx1=1;idx1<=n1;idx1++){
-            for(int idx2=1;idx2<=n2;idx2++){
-             if(text1.charAt(idx1-1)==text2.charAt(idx2-1)){
-                dp[idx1][idx2] = 1+dp[idx1-1][idx2-1];
-            }
-            else dp[idx1][idx2] = 0+Math.max(dp[idx1-1][idx2],dp[idx1][idx2-1]);   
-            }
-        }
-        
-        int len = dp[n1][n2];
-        int i=n1;
-        int j=n2;
-        int index = len-1;
-        String ans = "";
-        // for(int i=0;i<len; i++){
-        //     str+='$';
-        // }
-        
-        while(i>0&&j>0){
-            if(text1.charAt(i-1)==text2.charAt(j-1)){
-                ans+=text1.charAt(i-1);
-                index--;
-                i--;j--;
-            }
-            else if (dp[i-1][j]>dp[i][j-1]){
-                i--;
-            }
-            else j--;
-        }
-        System.out.println(ans);
-
-        
-        
-        // for(int i=0; i<=n1; i++){
-        //     for(int j=0;j<=n2;j++){
-        //         System.out.print(dp[i][j]);
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println(ans);
-        return dp[n1][n2];
+        Integer dp[][]  = new Integer[text1.length()][text2.length()];
+        return f(0,0,text1, text2,dp);
     }
 }
